d6a2c0cbfbb133bcf73861eb00da3ae00489a90e
Naemeh Adl <naemeh.adl@gmail.com>
2020-06-09 14:29:27 +0430

    Updates

---
+++ precache-manifest.dc05931323d03a3e2eabc4a84da9c890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e989981d59b0c6c8dea00db09971afb5",
+    "url": "/flowergarden/index.html"
+  },
+  {
+    "revision": "10346a06afc451ccb5fa",
+    "url": "/flowergarden/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c3083f16cbdbf99ceb3",
+    "url": "/flowergarden/static/css/main.c0a80790.chunk.css"
+  },
+  {
+    "revision": "10346a06afc451ccb5fa",
+    "url": "/flowergarden/static/js/2.8a72b117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flowergarden/static/js/2.8a72b117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3083f16cbdbf99ceb3",
+    "url": "/flowergarden/static/js/main.0a12e69f.chunk.js"
+  },
+  {
+    "revision": "a7e86c99f3b7e715100c",
+    "url": "/flowergarden/static/js/runtime-main.d3f3a490.js"
+  }
+]);
